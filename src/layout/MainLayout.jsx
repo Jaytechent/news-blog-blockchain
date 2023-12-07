60f64dbf20d6ee5d9082212bdab2e9661a441f3f
Jaytechent <fasasiolamilekan35@gmail.com>
2023-12-07 22:31:51 +0100

    update

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router";
+import Marquee from "react-fast-marquee";
 import Footer from "../components/Footer";
 import { HomeFilled } from "@ant-design/icons";
 const MainLayout = () => {
   return (
-    <div>
-      <Navbar />
+    <>
+      <div>
+        <Navbar />
+        <Marquee>
+          <div className="flex place-content-center font-extrabold ">
+            <HomeFilled />
+            <p className=" font-sans text-purple-950 ">THE GAWK HOUSE NEWS</p>
+          </div>
+        </Marquee>
 
-      <div className="flex place-content-center font-extrabold ">
-        <HomeFilled />
-        <p className=" font-sans text-purple-950 ">THE GAWK HOUSE NEWS</p>
+        <div className="flex flex-wrap justify-center"></div>
+
+        <Outlet />
+        <Footer />
       </div>
-      <div className="flex flex-wrap justify-center"></div>
-
-      <Outlet />
-      <Footer />
-    </div>
+    </>
   );
 };
 
